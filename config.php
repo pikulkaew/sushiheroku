c2630cf6c8bc1e753ba8a87099d211502073c75f
Kaew <kaew.ite@gmail.com>
2021-10-29 22:45:17 +0700

    add guestbook

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// Connect to server and select database.
+define('servername', 'us-cdbr-east-04.cleardb.com');
+define('username', 'b83ac594039c21');
+define('password', 'c84f6928');
+define('dbname', 'heroku_400ffed4efe9836');
+
+$objCon = mysqli_connect(servername, username, password,dbname);
+?>
+
