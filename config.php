f8604ca72119443df9cae057f25ba0df6cc1c76e
Kaew <kaew.ite@gmail.com>
2021-10-29 22:58:58 +0700

    add db

--- config.php
+++ config.php
@@ -1,9 +1,11 @@
 <?php
 // Connect to server and select database.
-define('servername', 'localhost');
-define('username', 'root');
-define('password', '');
-define('dbname', 'myhotel');
+define('servername', 'us-cdbr-east-04.cleardb.com');
+define('username', 'b917b1de3d63f8');
+define('password', '1c0244d1');
+define('dbname', 'heroku_44e6cddae72d8cc');
 
 $objCon = mysqli_connect(servername, username, password,dbname);
 ?>
+
+
